d0a7f0e7a936389d61251d7655a20ac5fc4eb5ea
Suman Paudel <dastonsuman1997@gmail.com>
2024-06-05 23:31:07 +0545

    some changes

--- bbox.py
+++ bbox.py
@@ -1,6 +1,8 @@
 from typing import NamedTuple
 import torch
 
+
+
 class BBox(NamedTuple):
     x1: float
     y1: float
@@ -10,4 +12,21 @@ class BBox(NamedTuple):
 
 # pascal VOC format
 # BoundingBox(x_min, y_min, x_max, y_max)
-# also know as (x1,y1,x2,y2)
+# also know as (x1,y1,x2,y2) (xyxy) format
+
+class COCO_bbox(NamedTuple):
+    x_min: float
+    y_min: float
+    width: float
+    heigth: float
+
+# coco format
+# xywh
+
+class CxCyWHBoundingBox(NamedTuple):
+    cx:float
+    cy:float
+    width: float
+    height:float
+# cxcywh values of these cordiantes are normalized
+# these are yolo format
